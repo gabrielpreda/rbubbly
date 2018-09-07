40653b6e2bbaf647fc5810133db54c0795609110
Gabriel Preda <gabriel.preda@endava.com>
2018-09-07 14:52:57 +0300

    fix library issue; refactor parameters names

--- R/rbubbly.R
+++ R/rbubbly.R
@@ -1,10 +1,18 @@
-library(plotly)
-library(gapminder)
+
+# rbubbly
+#
+# This package is used to plot animated and interactive bubble charts using Plotly
+# Inspiration: https://github.com/AashitaK/bubbly
+#
 
 
-rbubbly <- function(data, xcolumn, ycolumn, bubblecolumn, colorcolumn,
-                    slidercolumn, textcolumn, x_is_log=TRUE, y_is_log=FALSE,
-                    showSlider=TRUE) {
+rbubbly <- function(dataset, x_column, y_column, bubble_column, color_column,
+                    slider_column, text_column,
+                    x_title=NA, y_title=NA, title=NA,
+                    x_is_log=TRUE, y_is_log=FALSE,scale_bubble=1) {
+
+  library(plotly)
+  library(RColorBrewer)
 
   if(x_is_log)
     xtype="log"
@@ -16,24 +24,34 @@ rbubbly <- function(data, xcolumn, ycolumn, bubblecolumn, colorcolumn,
   else
     ytype=NA
 
+  xmin = min(x_column)
+  xmax = max(x_column)
+  ymin = min(y_column)
+  ymax = max(y_column)
+  smin = scale_bubble * sqrt(xmin*ymin)
+  smax = scale_bubble * sqrt(xmax*ymax)
   p <-
     plot_ly(data = data,
-      x = xcolumn,
-      y = ycolumn,
-      size = bubblecolumn,
-      color = colorcolumn,
-      frame = slidercolumn,
-      text = textcolumn,
-      hoverinfo = "text",
+      x = x_column,
+      y = y_column,
+      size = bubble_column,
+      color = color_column,
+      frame = slider_column,
+      text = text_column,
+      hoverinfo = "Country: ~text",
       type = 'scatter',
       mode = 'markers',
-      marker = list(opacity = 0.5),sizes = c(10, 500)
+      marker = list(opacity = 0.5),
+      sizes = c(smin, smax)
     ) %>%
     layout(
+      title = title,
       xaxis = list(
+        title = x_title,
         type = xtype
       ),
       yaxis = list(
+        title = y_title,
         type = ytype
       )
     )
